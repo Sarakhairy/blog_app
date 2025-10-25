c2bd591ca3c37dbdc04e057e1f10f6be603fc653
SaraKhairy <sarahkhairy180@gmail.com>
2025-10-25 14:14:57 +0200

    adding blog repository

---
+++ lib/features/blog/data/repositories/blog_repository_impl.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_app/features/blog/data/models/blog_model.dart';
+import 'package:blog_app/features/blog/domain/entities/blog_entity.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:fpdart/src/either.dart';
+import 'package:uuid/uuid.dart';
+
+class BlogrepositoryImpl implements BlogRepository {
+  final BlogRemoteDataSource remoteDataSource;
+
+  BlogrepositoryImpl({required this.remoteDataSource});
+  @override
+  Future<Either<Failure, BlogEntity>> uploadBlog({
+    required File image,
+    required String title,
+    required String content,
+    required String posterId,
+    required List<String> topics,
+  }) async {
+    try {
+      BlogModel blogModel = BlogModel(
+      id: Uuid().v1(),
+      posterId: posterId,
+      title: title,
+      content: content,
+      imageUrl: '',
+      topics: topics,
+      updatedAt: DateTime.now(),
+    );
+    final imageUrl = await remoteDataSource.uploadBlogImage(image: image, blog: blogModel);
+    
+
+    } on ServerException catch (e) {
+      return Left(Failure(e.message));
+    }
+  }
+}
